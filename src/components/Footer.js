79f86e09fe2351231c42bd7551c06c4130ac6768
Sean Chang <seantc@uci.edu>
2019-07-28 23:08:56 -0700

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,6 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
+              <li>Developed by Sean Chang</li>
               <li>&copy; Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
